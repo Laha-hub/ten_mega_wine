579f7adb2335f9be4653cacc6797de9801d5f4ed
Laha-hub <81339797 +Laha-hub@users.noreply.github.com>
2022-01-20 00:15:37 +0900

    #5 Make Progress Bar Work with JS

--- script.js
+++ script.js
@@ -3,17 +3,23 @@
 {
     let counter1 = 0; // スクロール正の値用
     let counter2 = 1; // スクロール負の値用
-    const sections = document.querySelectorAll('section');
+    let bool = true; // ページ最初、最後の目印
 
-    window.addEventListener('wheel', e => {
-        const deltaY = e.deltaY > 0;
-        if (deltaY) {
-            counter1++;
-            counter2++;
-        } else {
-            counter1--;
-            counter2--;
-        }
+    const sections = document.querySelectorAll('section');
+    const progress = document.querySelector('.progress h2');
+    const circles = document.querySelectorAll('.circle');
+
+    const progressCounter = () => {
+        progress.textContent = `${counter2}/${sections.length}`;
+
+        circles.forEach(circle => {
+            circle.style.backgroundColor = 'transparent';
+        });
+        document.querySelector(`.circle-${counter2}`).style.backgroundColor = '#ddd';
+    };
+
+    const pageController = () => {
+        bool = true;
 
         if (counter1 === 5) {
             sections.forEach(section => {
@@ -21,7 +27,8 @@
             });
             counter1 = 0;
             counter2 = 1;
-            return;
+            progressCounter();
+            bool = false;
         }
 
         if (counter1 === -1) {
@@ -33,9 +40,52 @@
             });
             counter1 = 4;
             counter2 = 5;
+            progressCounter();
+            bool = false;
+        }
+        progressCounter();
+        return bool;
+    };
+
+    window.addEventListener('wheel', e => {
+        const deltaY = e.deltaY > 0;
+        if (deltaY) {
+            counter1++;
+            counter2++;
+        } else {
+            counter1--;
+            counter2--;
         }
 
-        document.querySelector(`.section-${deltaY ? counter1 : counter2}`).style.left = `${deltaY ? '-100vw' : '0'}`;
+        pageController();
 
+        // bool === false（最初、最終ページ）のときは、何もせずに終わる。bool === trueのときはページ変更
+        bool && (document.querySelector(`.section-${deltaY ? counter1 : counter2}`).style.left = `${deltaY ? '-100vw' : '0'}`);
+
+    });
+
+
+    // 左ボタン
+    document.querySelector('.left-btn').addEventListener('click', () => {
+        counter1--;
+        counter2--;
+        pageController() && (document.querySelector(`.section-${counter2}`).style.left = '0');
+    });
+
+    // 右ボタン
+    document.querySelector('.right-btn').addEventListener('click', () => {
+        counter1++;
+        counter2++;
+        pageController() && (document.querySelector(`.section-${counter1}`).style.left = '-100vw');
+    });
+
+
+    // .section-3-wrapper アニメーション
+    document.querySelector('.grapes-img').addEventListener('mouseover', () => {
+        document.querySelector('.section-3-wrapper').style.opacity = '.8';
+    });
+
+    document.querySelector('.grapes-img').addEventListener('mouseout', () => {
+        document.querySelector('.section-3-wrapper').style.opacity = '1';
     });
 }
